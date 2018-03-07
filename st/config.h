749bfbabad425c69ecf698471099fc78240d6060
Jacob Cherry <jacobcherry36@gmail.com>
2018-03-07 03:39:44 -0600

    Let's see what happens

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "monospace:pixelsize=16";
+char font[] = "monospace:size=12";
 int borderpx = 2;
 
 /*

f94b6a56db9e5710b0678a9279add0e66644ae8b
Jacob Cherry <jacobcherry36@gmail.com>
2018-03-07 03:36:13 -0600

    Make font bigger and make terminal more transparent

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "monospace:pixelsize=12";
+char font[] = "monospace:pixelsize=14";
 int borderpx = 2;
 
 /*
@@ -83,7 +83,7 @@ char termname[] = "st-256color";
 static unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0x60;
+unsigned int alpha = 0x40;
 
 /* Terminal colors (16 first used in escape sequence) */
 const char *colorname[] = {

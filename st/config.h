401aee71c85b86b8c6a435e797b8ad2005ef21e0
Jacob Cherry <jacobcherry36@gmail.com>
2018-03-07 03:40:21 -0600

    Test2

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "monospace:size=12";
+char font[] = "monospace:size=20";
 int borderpx = 2;
 
 /*
